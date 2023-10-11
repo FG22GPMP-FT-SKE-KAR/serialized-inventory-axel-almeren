ec5d7f14c08d7c52e4f3dbf83b953c4cea9aba91
AxelAEDU <axel.almeren@edu.futuregames.nu>
2023-10-11 11:04:11 +0200

    update ,added clicker script. changes to end screen for images with health

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    public void ClickedHealth()
+    {
+        Debug.Log("Health Button Clicked");
+    }
+
+    public void ClickedArmor()
+    {
+        Debug.Log("Armor Button Clicked");
+    }
+    public void ClickedSpeed()
+    {
+        Debug.Log("Speed Button Clicked");
+    }
+}
